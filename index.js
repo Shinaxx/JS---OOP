b460eadec2e5b96004b0686b664df1c638295b91
shinaz <shinattey10@gmail.com>
2021-09-21 15:28:39 +0500

    factory function

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
-        console.log('draw');
-    }
-};
+// factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
 
+const circle = createCircle(1);
 circle.draw();
+
