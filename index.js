d626bd4edac6416af9e67fd484849db1db10357c
shinaz <shinattey10@gmail.com>
2021-09-21 16:29:55 +0500

    private properties and methods

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 function Circle(radius) {
     this.radius = radius;
 
-    this.defaultLocation = {x: 0, y: 0 };
+    let defaultLocation = {x: 0, y: 0 };
 
-    this.computeOptimumLocation = function(factor) {
+    let computeOptimumLocation = function(factor) {
         // ...
     }
     
     this.draw = function() {
-        this.computeOptimumLocation(0.1);
+        computeOptimumLocation(0.1);
 
         console.log('draw');
     };
